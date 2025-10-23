a474ab47535e3f150185cfe397556b581ce500db
Rohan Choudhary <rohan.c2002@gmail.com>
2025-10-23 17:58:04 +0530

    create factory contract

--- test/Factory.js
+++ test/Factory.js
@@ -3,5 +3,28 @@ const { expect } = require("chai")
 const { ethers } = require("hardhat")
 
 describe("Factory", function () {
+    const FEE = ethers.parseUnits("0.01", 18)
+
+    async function deployFactoryFixture() {
+        const [deployer] = await ethers.getSigners()
+
+        const Factory = await ethers.getContractFactory("Factory")
+        const factory = await Factory.deploy(FEE);
+
+        return { factory, deployer }
+    }
+
+    describe("Deployment", function () {
+        it("should set the fee", async function () {
+            const { factory } = await loadFixture(deployFactoryFixture)
+            expect(await factory.fee()).to.equal(FEE)
+        })
+
+        it("should set the owner", async function () {
+            const { factory, deployer } = await loadFixture(deployFactoryFixture)
+            expect(await factory.owner()).to.equal(deployer.address)
+        })
+    })
 
 })
+
